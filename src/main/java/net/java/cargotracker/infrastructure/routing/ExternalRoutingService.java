7c0781a875c0cc3c3fa4c5c09fd390d9aae48191
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2018-05-29 13:05:22 +0200

    Refactoring for Java EE 8 Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@payara.fish>

--- src/main/java/net/java/cargotracker/infrastructure/routing/ExternalRoutingService.java
+++ src/main/java/net/java/cargotracker/infrastructure/routing/ExternalRoutingService.java
@@ -3,13 +3,11 @@ package net.java.cargotracker.infrastructure.routing;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
-import javax.enterprise.concurrent.ContextService;
 import javax.inject.Inject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -17,9 +15,6 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import net.java.cargotracker.application.internal.ApplicationInfo;
-import javax.ws.rs.client.*;
-import javax.ws.rs.core.*;
-import net.java.cargotracker.application.util.JsonMoxyConfigurationContextResolver;
 import net.java.cargotracker.domain.model.cargo.Itinerary;
 import net.java.cargotracker.domain.model.cargo.Leg;
 import net.java.cargotracker.domain.model.cargo.RouteSpecification;
@@ -30,7 +25,6 @@ import net.java.cargotracker.domain.model.voyage.VoyageRepository;
 import net.java.cargotracker.domain.service.RoutingService;
 import net.java.pathfinder.api.TransitEdge;
 import net.java.pathfinder.api.TransitPath;
-import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 
 /**
  * Our end of the routing service. This is basically a data model translation
@@ -55,8 +49,6 @@ public class ExternalRoutingService implements RoutingService {
             ExternalRoutingService.class.getName());
     @Inject
     private ApplicationInfo appInfo;
-    @Resource
-    ContextService contextService;
 
     @PostConstruct
     public void init() {
@@ -66,8 +58,6 @@ public class ExternalRoutingService implements RoutingService {
         } else {
             graphTraversalResource = jaxrsClient.target(graphTraversalUrl);
         }
-        graphTraversalResource.register(new MoxyJsonFeature()).register(
-                new JsonMoxyConfigurationContextResolver());
     }
 
     @Override
@@ -78,17 +68,13 @@ public class ExternalRoutingService implements RoutingService {
         String destination = routeSpecification.getDestination().getUnLocode()
                 .getIdString();
 
-        CompletableFuture<List<Itinerary>> futureResult = new CompletableFuture<>();
-        graphTraversalResource
+        CompletionStage<List<Itinerary>> futureResult = graphTraversalResource
                 .queryParam("origin", origin)
                 .queryParam("destination", destination)
                 .request(MediaType.APPLICATION_JSON_TYPE)
-                .async()
-                .get(new InvocationCallback<List<TransitPath>>() {
-                    
-                    Consumer<List<TransitPath>> onCompleted = contextService.createContextualProxy(new Consumer<List<TransitPath>>() {
-                        @Override
-                        public void accept(List<TransitPath> transitPaths) {
+                .rx()
+                .get(new GenericType<List<TransitPath>>() {})
+                .thenApply(transitPaths -> {
                             // The returned result is then translated back into our domain model.
                             List<Itinerary> itineraries = new ArrayList<>();
 
@@ -102,26 +88,11 @@ public class ExternalRoutingService implements RoutingService {
                                             "Received itinerary that did not satisfy the route specification");
                                 }
                             }
-
-                            futureResult.complete(itineraries);
-                        }
-
-                    }, Consumer.class);
-
-                    @Override
-                    public void completed(List<TransitPath> transitPaths) {
-                        onCompleted.accept(transitPaths);
-                    }
-
-                    @Override
-                    public void failed(Throwable throwable) {
-                        futureResult.completeExceptionally(throwable);
-                    }
-
+                            return itineraries;
                 });
 
         try {
-            return futureResult.get(300, TimeUnit.SECONDS);
+            return futureResult.toCompletableFuture().get(300, TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException ex) {
             throw new RuntimeException(ex);
         }
